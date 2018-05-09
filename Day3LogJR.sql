4015e8afac4f29f9c6d380370d3ab13edb1f5834
joaorebelo92 <joao.rebelo92@gmail.com>
2018-05-09 11:35:01 -0400

    Add files via upload

---
+++ Day3LogJR.sql
@@ -0,0 +1,42 @@
+MariaDB [(none)]> CREATE DATABASE CSD2204S18;
+ERROR 1007 (HY000): Can't create database 'csd2204s18'; database exists
+MariaDB [(none)]> show databases;
++--------------------+
+| Database           |
++--------------------+
+| csd2204s18         |
+| information_schema |
+| mysql              |
+| performance_schema |
+| phpmyadmin         |
+| test               |
++--------------------+
+6 rows in set (0.00 sec)
+
+MariaDB [(none)]> CREATE TABLE Customer(
+    -> custId VARCHAR(5) PRIMARY KEY,
+    -> name VARCHAR(50),
+    -> nickname VARCHAR(10),
+    -> city VARCHAR(20),
+    -> postalCode VARCHAR(10),
+    -> age INTEGER(3));
+ERROR 1046 (3D000): No database selected
+MariaDB [(none)]> USE CSD2204S18;
+Database changed
+MariaDB [CSD2204S18]> CREATE TABLE Customer(
+    -> custId VARCHAR(5) PRIMARY KEY,
+    -> name VARCHAR(50),
+    -> nickname VARCHAR(10),
+    -> city VARCHAR(20),
+    -> postalCode VARCHAR(10),
+    -> age INTEGER(3));
+Query OK, 0 rows affected (0.35 sec)
+
+MariaDB [CSD2204S18]> SHOW TABLES;
++----------------------+
+| Tables_in_csd2204s18 |
++----------------------+
+| customer             |
++----------------------+
+1 row in set (0.00 sec)
+
